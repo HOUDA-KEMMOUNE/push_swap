160ff8c95629fc7179f57e55632cccc4e813e189
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2025-02-02 13:25:14 +0100

    push_swap

---
+++ algos.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algos.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 13:09:54 by hkemmoun          #+#    #+#             */
+/*   Updated: 2025/02/02 13:09:57 by hkemmoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    algo_for_two(t_stack **head_a)
+{
+    t_stack *ptr_a;
+
+    if (!(*head_a))
+        return ;
+    ptr_a = *head_a;
+    if (ptr_a-> data > ptr_a->next->data)
+    {
+        sa(head_a);
+        write(1, "sa\n", 3);
+    }
+    else
+        return ;
+}

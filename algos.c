bf351a77ad1ec7c3b6094d9b0152cc8f6b63f7d3
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2025-02-03 04:16:50 +0100

    push_swap

--- algos.c
+++ algos.c
@@ -53,7 +53,17 @@ void    algo_for_three(t_stack **head_a)
 	else if ((ptr_a->data == max) && (ptr_a->next->data == min))
 	{
 		ra(head_a);
-		write(1, "sa\n", 3);
+		write(1, "ra\n", 3);
 	}
-	// ft_print_node(*head_a);
+	else if ((ptr_a->data == min) && (ptr_a->next->data == max))
+	{
+		sa(head_a);
+		ra(head_a);
+		write(1, "sa\nra\n", 6);
+	}
+	else
+	{
+		rra(head_a);
+		write(1, "rra\n", 4);
+	}
 }
